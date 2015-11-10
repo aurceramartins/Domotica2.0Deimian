5ffab829030f7aa1e1382179fe1fd0328f52f197
aurceramartins <alexurcera23@gmail.com>
2015-11-10 10:58:48 +0100

    TrippDaWorlddd

--- app/src/main/java/com/example/alex/domotica20/MainActivity.java
+++ app/src/main/java/com/example/alex/domotica20/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private Camera camera;
     private Window window;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
